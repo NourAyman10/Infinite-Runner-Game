55db439749b15236435a2a4f911a6091e7d709c1
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2024-05-11 02:31:13 +0300

    add AI dragon Chasing

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -8,7 +8,7 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(50 * Time.deltaTime, 0, 0);
+        transform.Rotate(80 * Time.deltaTime, 0, 0);
     }
 
     private void OnTriggerEnter(Collider other)
